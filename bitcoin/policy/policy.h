c3116377a71a8fd2d21c4c7fdc3ef5464bf25ac2
Bruno Garcia <brunoely.gc@gmail.com>
2023-11-10 19:17:55 -0300

    add bitcoin core for miniscript

---
+++ bitcoin/policy/policy.h
@@ -0,0 +1,9 @@
+#ifndef BITCOIN_POLICY_POLICY_H
+#define BITCOIN_POLICY_POLICY_H
+
+static const unsigned int MAX_STANDARD_P2WSH_STACK_ITEMS = 100;
+
+/** The maximum size in bytes of a standard witnessScript */
+static const unsigned int MAX_STANDARD_P2WSH_SCRIPT_SIZE = 3600;
+
+#endif
