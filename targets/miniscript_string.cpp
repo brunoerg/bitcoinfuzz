4c16d298906861c5ab6d29699af92d03ce31d0a9
Bruno Garcia <brunoely.gc@gmail.com>
2023-11-16 19:48:59 -0300

    fix miniscript_string

--- targets/miniscript_string.cpp
+++ targets/miniscript_string.cpp
@@ -9,14 +9,18 @@
 
 extern "C" bool rust_miniscript_from_str(std::string& miniscript_str);
 
+bool BitcoinCore(const std::string& input_str)
+{
+    auto ret{miniscript::FromString(input_str, COMPILER_CTX)};
+    if (!ret || !ret->IsValidTopLevel()) return false;
+    return true;
+}
+
 void MiniscriptFromString(FuzzedDataProvider& provider) 
 {
-    std::string input_str{provider.ConsumeBytesAsString(provider.remaining_bytes() - 1)};
-    if (input_str.size() == 1) return;
-    auto ret{miniscript::FromString(input_str, COMPILER_CTX)};
-    auto conditions = ret && ret->IsSane();
-    std::cout << conditions << std::endl;
-    std::cout << input_str << std::endl;
-    assert(rust_miniscript_from_str(input_str) == conditions);
+    std::string input_str{provider.ConsumeRemainingBytesAsString().c_str()};
+    bool core{BitcoinCore(input_str)};
+    bool rust_miniscript{rust_miniscript_from_str(input_str)};
+    if (rust_miniscript) assert(core);
 }
 
