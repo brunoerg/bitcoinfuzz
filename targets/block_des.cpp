279b3a171bdd2a8363f4cf3df131c0a1c8143cb4
Bruno Garcia <brunoely.gc@gmail.com>
2024-04-09 03:39:39 -0300

    fix and improvements on block deserialization target

--- targets/block_des.cpp
+++ targets/block_des.cpp
@@ -14,7 +14,7 @@ bool BlockDesCore(Span<const uint8_t> buffer)
     DataStream ds{buffer};
     CBlock block;
     try {
-        ds >> TX_NO_WITNESS(block);
+        ds >> TX_WITH_WITNESS(block);
         //block.GetHash();
     } catch (const std::ios_base::failure&) {
         return false;
@@ -22,11 +22,13 @@ bool BlockDesCore(Span<const uint8_t> buffer)
     return true;
 }
 
+// This target is expected to crash, needs some verification (e.g. segwit version).
 void BlockDes(FuzzedDataProvider& provider) 
 {
     std::vector<uint8_t> buffer{provider.ConsumeRemainingBytes<uint8_t>()};
     // TODO: Compare the `block hash`
     bool core{BlockDesCore(buffer)};
     std::string rust_bitcoin{rust_bitcoin_des_block(buffer.data(), buffer.size())};
-    if (rust_bitcoin != "") assert(core);
+    if (core) assert(rust_bitcoin != "");
+    else assert(rust_bitcoin == "");
 }
