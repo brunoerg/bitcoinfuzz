df8b5096b0a094f57c4c25766780f6268e39ada5
Kartik Agarwala <agarwala.kartik@gmail.com>
2024-06-02 09:02:58 +0530

    Add support for comparing block hashes

--- targets/block_des.cpp
+++ targets/block_des.cpp
@@ -10,29 +10,36 @@
 extern "C" char* rust_bitcoin_des_block(const uint8_t *data, size_t len);
 extern "C" char* go_btcd_des_block(uint8_t *data, size_t len);
 
-bool BlockDesCore(Span<const uint8_t> buffer)
+std::string BlockDesCore(Span<const uint8_t> buffer)
 {
     DataStream ds{buffer};
     CBlock block;
     try {
         ds >> TX_WITH_WITNESS(block);
-        //block.GetHash();
+        return block.GetHash().ToString();
     } catch (const std::ios_base::failure&) {
-        return false;
+        return "";
     }
-    return true;
+    return "";
 }
 
 // This target is expected to crash, needs some verification (e.g. segwit version).
 void BlockDes(FuzzedDataProvider& provider) 
 {
     std::vector<uint8_t> buffer{provider.ConsumeRemainingBytes<uint8_t>()};
-    // TODO: Compare the `block hash`
-    bool core{BlockDesCore(buffer)};
+
+    std::string core{BlockDesCore(buffer)};
     std::string rust_bitcoin{rust_bitcoin_des_block(buffer.data(), buffer.size())};
     std::string go_btcd{go_btcd_des_block(buffer.data(), buffer.size())};
 
+    // Uncomment this to print the block hashes
+    //std::cout << "Core: " << core << "\n";
+    //std::cout << "Rust: " << rust_bitcoin << "\n";
+    //std::cout << "Go: " << go_btcd << "\n";
+
     if (rust_bitcoin == "unsupported segwit version") return;
-    if (core) assert(rust_bitcoin != "" || go_btcd != "");
+    if (core != "") assert(rust_bitcoin != "" || go_btcd != "");
     else assert(rust_bitcoin == "" || go_btcd == "");
+
+    assert(go_btcd == rust_bitcoin && rust_bitcoin == core);
 }
