35d80d6a82e706178de8fedf670834e27e7db1e9
Bruno Garcia <brunoely.gc@gmail.com>
2024-06-04 16:29:41 -0300

    add target for tx deserialization

--- btcd_lib/wrapper.go
+++ btcd_lib/wrapper.go
@@ -20,6 +20,19 @@ func go_btcd_des_block(cinput *C.uchar, len C.int) *C.char {
 	return C.CString("")
 }
 
+//export go_btcd_des_tx
+func go_btcd_des_tx(cinput *C.uchar, len C.int) *C.char {
+
+	data := C.GoBytes(unsafe.Pointer(cinput), len)
+
+	b, err := btcutil.NewTxFromBytes(data)
+	if err == nil {
+		return C.CString(b.Hash().String())
+	}
+
+	return C.CString("")
+}
+
 func main() {
 
 }
